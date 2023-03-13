a4fd2cc21e5d9cc3fed4f53d487141637bf81d30
Bill Wallace <wayfarer3130@gmail.com>
2023-03-13 17:14:14 -0400

    fix: Don't throw an exception when nothing to jump to

--- extensions/cornerstone/src/Viewport/OHIFCornerstoneViewport.tsx
+++ extensions/cornerstone/src/Viewport/OHIFCornerstoneViewport.tsx
@@ -553,6 +553,7 @@ function _checkForCachedJumpToMeasurementEvents(
   const displaysUIDs = displaySets.map(
     displaySet => displaySet.displaySetInstanceUID
   );
+  if (!displaysUIDs?.length) return;
 
   const measurementIdToJumpTo = measurementService.getJumpToMeasurement(
     viewportIndex
@@ -564,7 +565,7 @@ function _checkForCachedJumpToMeasurementEvents(
       measurementIdToJumpTo
     );
 
-    if (displaysUIDs.includes(measurement.displaySetInstanceUID)) {
+    if (displaysUIDs.includes(measurement?.displaySetInstanceUID)) {
       _jumpToMeasurement(
         measurement,
         elementRef,
